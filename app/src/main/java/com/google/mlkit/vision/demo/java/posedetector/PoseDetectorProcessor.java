50ca0f89945379ed56470876fd5181cc83d6e3bb
siriusBl4ck <sauravkale2019@gmail.com>
2021-01-31 09:37:48 +0530

    initial

---
+++ app/src/main/java/com/google/mlkit/vision/demo/java/posedetector/PoseDetectorProcessor.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2020 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.mlkit.vision.demo.java.posedetector;
+
+import android.content.Context;
+import androidx.annotation.NonNull;
+import android.util.Log;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.demo.GraphicOverlay;
+import com.google.mlkit.vision.demo.java.VisionProcessorBase;
+import com.google.mlkit.vision.pose.Pose;
+import com.google.mlkit.vision.pose.PoseDetection;
+import com.google.mlkit.vision.pose.PoseDetector;
+import com.google.mlkit.vision.pose.PoseDetectorOptionsBase;
+
+/** A processor to run pose detector. */
+public class PoseDetectorProcessor extends VisionProcessorBase<Pose> {
+
+  private static final String TAG = "PoseDetectorProcessor";
+
+  private final PoseDetector detector;
+
+  private final boolean showInFrameLikelihood;
+  private final boolean visualizeZ;
+  private final boolean rescaleZForVisualization;
+
+  public PoseDetectorProcessor(
+      Context context,
+      PoseDetectorOptionsBase options,
+      boolean showInFrameLikelihood,
+      boolean visualizeZ,
+      boolean rescaleZForVisualization) {
+    super(context);
+    this.showInFrameLikelihood = showInFrameLikelihood;
+    this.visualizeZ = visualizeZ;
+    this.rescaleZForVisualization = rescaleZForVisualization;
+    detector = PoseDetection.getClient(options);
+  }
+
+  @Override
+  public void stop() {
+    super.stop();
+    detector.close();
+  }
+
+  @Override
+  protected Task<Pose> detectInImage(InputImage image) {
+    return detector.process(image);
+  }
+
+  @Override
+  protected void onSuccess(@NonNull Pose pose, @NonNull GraphicOverlay graphicOverlay) {
+    graphicOverlay.add(
+        new PoseGraphic(
+            graphicOverlay, pose, showInFrameLikelihood, visualizeZ, rescaleZForVisualization));
+  }
+
+  @Override
+  protected void onFailure(@NonNull Exception e) {
+    Log.e(TAG, "Pose detection failed!", e);
+  }
+}
